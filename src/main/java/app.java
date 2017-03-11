eb4b10591f0ef18de4d24c2df81b1f68ca0507e8
DenisLaptev <laptev.denis.it@mail.ru>
2017-03-11 12:03:21 +0200

    create method print

--- src/main/java/app.java
+++ src/main/java/app.java
@@ -13,7 +13,7 @@ public class app {
 
         for (int i = 0; i < books.length; i++) {
             System.out.println(books[i]);
-            books[i].print();
+            //books[i].print();
         }
 
     }
